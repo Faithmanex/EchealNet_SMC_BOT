47b8dda410448c109302f7c8445b58f2584cc060
kemzzy-dev <ekemini.udongow11@gmail.com>
2024-05-07 00:49:16 +0100

    refactored some code

--- strategy.py
+++ strategy.py
@@ -63,61 +63,62 @@ class Strategy:
             plot_df["Entry"] - plot_df["Stop_Loss"]
         )
 
-        return self.new_df
+        return plot_df
 
 
     def AMSstrategy(self, RR):
-       for x in range(self.new_df.shape[0]):
-        if (
-            (
-                pd.notna(self.new_df.iloc[x - 0]["Is_Low"])
-                and pd.notna(self.new_df.iloc[x - 1]["Is_High"])
-                and pd.notna(self.new_df.iloc[x - 2]["Is_Low"])
-                and pd.notna(self.new_df.iloc[x - 3]["Is_High"])
-                and pd.notna(self.new_df.iloc[x - 4]["Is_Low"])
-                and pd.notna(self.new_df.iloc[x - 5]["Is_High"])
-            )
-            and (
-                (self.new_df.iloc[x - 0]["Is_Low"]) < (self.new_df.iloc[x - 2]["Is_Low"])
-                and (self.new_df.iloc[x - 1]["Is_High"]) > (self.new_df.iloc[x - 2]["Is_Low"])
-                and (self.new_df.iloc[x - 3]["Is_High"]) > (self.new_df.iloc[x - 1]["Is_High"])
-                and (self.new_df.iloc[x - 5]["Is_High"]) < (self.new_df.iloc[x - 3]["Is_High"])
-                and (self.new_df.iloc[x - 4]["Is_Low"]) < (self.new_df.iloc[x - 2]["Is_Low"])
-                # and (new_df.iloc[x - 3]["Is_High"]) < (new_df.iloc[x - 1]["Is_High"])
-                # and (new_df.iloc[x - 4]["Is_Low"]) < (new_df.iloc[x - 3]["Is_Low"])
-            )
-            and (
-                (self.new_df.iloc[x - 2]["Is_Low"]) - (self.new_df.iloc[x - 0]["Is_Low"])
-                > (
-                    (
-                        100
-                        / 100
-                        * (self.new_df.iloc[x - 1]["Is_High"] - self.new_df.iloc[x - 2]["Is_Low"])
+       
+        for x in range(self.new_df.shape[0]):
+            if (
+                (
+                    pd.notna(self.new_df.iloc[x - 0]["Is_Low"])
+                    and pd.notna(self.new_df.iloc[x - 1]["Is_High"])
+                    and pd.notna(self.new_df.iloc[x - 2]["Is_Low"])
+                    and pd.notna(self.new_df.iloc[x - 3]["Is_High"])
+                    and pd.notna(self.new_df.iloc[x - 4]["Is_Low"])
+                    and pd.notna(self.new_df.iloc[x - 5]["Is_High"])
+                )
+                and (
+                    (self.new_df.iloc[x - 0]["Is_Low"]) < (self.new_df.iloc[x - 2]["Is_Low"])
+                    and (self.new_df.iloc[x - 1]["Is_High"]) > (self.new_df.iloc[x - 2]["Is_Low"])
+                    and (self.new_df.iloc[x - 3]["Is_High"]) > (self.new_df.iloc[x - 1]["Is_High"])
+                    and (self.new_df.iloc[x - 5]["Is_High"]) < (self.new_df.iloc[x - 3]["Is_High"])
+                    and (self.new_df.iloc[x - 4]["Is_Low"]) < (self.new_df.iloc[x - 2]["Is_Low"])
+                    # and (new_df.iloc[x - 3]["Is_High"]) < (new_df.iloc[x - 1]["Is_High"])
+                    # and (new_df.iloc[x - 4]["Is_Low"]) < (new_df.iloc[x - 3]["Is_Low"])
+                )
+                and (
+                    (self.new_df.iloc[x - 2]["Is_Low"]) - (self.new_df.iloc[x - 0]["Is_Low"])
+                    > (
+                        (
+                            100
+                            / 100
+                            * (self.new_df.iloc[x - 1]["Is_High"] - self.new_df.iloc[x - 2]["Is_Low"])
+                        )
                     )
                 )
-            )
-        ):
+            ):
 
-            # if (df.iloc[x-0,2] == 'low' and df.iloc[x-1,2] == 'high' and df.iloc[x-2,2] == 'low' and df.iloc[x-3,2] == 'high' and df.iloc[x-4,2] == 'low' and df.iloc[x-5,2] == 'high') and (df.iloc[x-0,1] < df.iloc[x-4,1]) and (df.iloc[x-1,1] > df.iloc[x-3,1]) and (df.iloc[x-3,1] > df.iloc[x-5,1]) and (df.iloc[x-5,1] > df.iloc[x-4,1]) and (df.iloc[x-4,1] < df.iloc[x-2,1]) and ((df.iloc[x-4,1] -df.iloc[x-0,1]) > 40/100 * (df.iloc[x-1,1] - df.iloc[x-2,1])):
-            # TODO: Write code to check columns "Is_High" and "Is_Low" for where either Is_High or Is_Low is True, then check if Is_Low is True and Is_High is False and vice versa, then create a new dataframe with the results with the values of the dataframe df
+                # if (df.iloc[x-0,2] == 'low' and df.iloc[x-1,2] == 'high' and df.iloc[x-2,2] == 'low' and df.iloc[x-3,2] == 'high' and df.iloc[x-4,2] == 'low' and df.iloc[x-5,2] == 'high') and (df.iloc[x-0,1] < df.iloc[x-4,1]) and (df.iloc[x-1,1] > df.iloc[x-3,1]) and (df.iloc[x-3,1] > df.iloc[x-5,1]) and (df.iloc[x-5,1] > df.iloc[x-4,1]) and (df.iloc[x-4,1] < df.iloc[x-2,1]) and ((df.iloc[x-4,1] -df.iloc[x-0,1]) > 40/100 * (df.iloc[x-1,1] - df.iloc[x-2,1])):
+                # TODO: Write code to check columns "Is_High" and "Is_Low" for where either Is_High or Is_Low is True, then check if Is_Low is True and Is_High is False and vice versa, then create a new dataframe with the results with the values of the dataframe df
 
-            self.occurences.append(self.new_df.index[x - 0])
-            block_range = (self.new_df.iloc[x - 3]["Is_High"]) - (self.new_df.iloc[x - 4]["Is_Low"])
+                self.occurences.append(self.new_df.index[x - 0])
+                block_range = (self.new_df.iloc[x - 3]["Is_High"]) - (self.new_df.iloc[x - 4]["Is_Low"])
 
-            # entry_price = block_range * 0.236 + df.iloc[x - 2, 1]
-            entry_price = self.new_df.iloc[x - 5]["Is_High"]
-            # (float((df.iloc[x-1,1] - df.iloc[x-2,1])/4) + df.iloc[x-2,1])
-            self.entries.append(entry_price)
+                # entry_price = block_range * 0.236 + df.iloc[x - 2, 1]
+                entry_price = self.new_df.iloc[x - 5]["Is_High"]
+                # (float((df.iloc[x-1,1] - df.iloc[x-2,1])/4) + df.iloc[x-2,1])
+                self.entries.append(entry_price)
 
-            # Calculate the stop loss and take profit levels
-            stop_loss = (self.new_df.iloc[x - 5]["Is_High"]) + (
-                block_range * 1
-            )  # 50% retracement from entry
+                # Calculate the stop loss and take profit levels
+                stop_loss = (self.new_df.iloc[x - 5]["Is_High"]) + (
+                    block_range * 1
+                )  # 50% retracement from entry
 
-            take_profit = entry_price - ((stop_loss - entry_price) * RR)
+                take_profit = entry_price - ((stop_loss - entry_price) * RR)
 
-            self.stop_losses.append(stop_loss)
-            self.take_profits.append(take_profit)
+                self.stop_losses.append(stop_loss)
+                self.take_profits.append(take_profit)
 
         # Create a DataFrame with the results
         plot_df = pd.DataFrame(
